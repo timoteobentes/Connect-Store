2bc11adc282ae8eb272ef5cf13cfb20f50239cb3
timoteobentes <bentestimoteo@gmail.com>
2021-12-08 19:02:11 -0400

    loja virtual_exercitando frontend

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function clicou() {
+    alert("Tem certeza?")
+}
+
+function quero() {
+    alert("Parabéns por entrar!")
+}
+
+function camisa() {
+    alert("Sem conteúdo!!!!")
+}
+
+function calca() {
+    alert("Sem conteúdo!!!!")
+}
+
+function tenis() {
+    alert("Sem conteúdo!!!!")
+}
+
+function bones() {
+    alert("Sem conteúdo!!!!")
+}
